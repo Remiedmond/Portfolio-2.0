398525a637e40cfff040a2a56c014f504b0090f3
remiedmond <remi.emond6230@gmail.com>
2025-11-18 16:06:31 +0100

    modification de test pour voir si avec preprocess dans svelte.config.js ça fonctionne

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,27 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        }),
-        paths: {
-		base: process.env.NODE_ENV === 'production' ? '/Portfolio-2.0' : ''
-		},
-        prerender: {
-            handleHttpError: 'warn',
-            handleMissingId: 'warn',
-            entries: ['*'] // Prerender toutes les routes découvertes
-        }
+  preprocess: vitePreprocess(), // AJOUTEZ CETTE LIGNE
+  
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/Portfolio-2.0' : ''
+    },
+    prerender: {
+      handleHttpError: 'warn',
+      handleMissingId: 'warn',
+      entries: ['*']
     }
+  }
 };
 
 export default config;
