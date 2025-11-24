b190c5017cb1092eb5a8d8db1ccdbc9d24f20304
remiedmond <remi.emond6230@gmail.com>
2025-11-24 12:01:46 +0100

    Fix navigation links with base path

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,12 +1,9 @@
-<!-- Header.svelte -->
 <script>
   import './Header.css';
   import logo from '../../img/logo.png';
+  import { base } from '$app/paths';
      
-  // Props (propriétés reçues du parent)
   export let title = "Mon Portfolio";
-     
-  // Variables locales
   let isMenuOpen = false;
      
   function toggleMenu() {
@@ -14,17 +11,15 @@
   }
 </script>
 
-<!-- HTML du composant -->
 <header>
   <nav>
     <div class="Logoaccueil">
-      <a href="/">
+      <a href="{base}/">
         <img src={logo} class="logo1" alt="Logo de Rémi EDMOND">
         <h1>{title}</h1>
       </a>
     </div>
 
-    <!-- Menu burger button -->
     <button class="burger-menu" class:open={isMenuOpen} on:click={toggleMenu}>
       <div class="burger-line"></div>
       <div class="burger-line"></div>
@@ -33,16 +28,16 @@
 
     <ul class:open={isMenuOpen}>
       <div class="scene">
-        <a href="/about" class="panel profil" on:click={() => isMenuOpen = false}>
+        <a href="{base}/about" class="panel profil" on:click={() => isMenuOpen = false}>
           <li>Mon Profil</li>
         </a>
-        <a href="/projects" class="panel web" on:click={() => isMenuOpen = false}>
+        <a href="{base}/projects" class="panel web" on:click={() => isMenuOpen = false}>
           <li>WEB</li>
         </a>
-        <a href="/design" class="panel design" on:click={() => isMenuOpen = false}>
+        <a href="{base}/design" class="panel design" on:click={() => isMenuOpen = false}>
           <li>Design graphique</li>
         </a>
-        <a href="/contact" class="panel contact" on:click={() => isMenuOpen = false}>
+        <a href="{base}/contact" class="panel contact" on:click={() => isMenuOpen = false}>
           <li>Contact</li>
         </a>
       </div>
